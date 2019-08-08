US-Thakur/merge_testing

commit 4e6bde8433abf86963a01e20b648edd24e45d74d

Fix: (--select) Don't iterate over every heading with preamble
Oops, using "when" instead of "while" did outline-next-heading even
when re-search-forward found no more matches.  That unnecessarily
slowed down some searches, negating almost all of the gains.

dir2/org-ql.el
@@ -404,10 +404,11 @@ If NARROW is non-nil, buffer will not be widened."
               (goto-char (point-min))
               (when (org-before-first-heading-p)
                 (outline-next-heading))
-              (cond (preamble-re (cl-loop when (and (when (re-search-forward preamble-re nil t)
-                                                      (outline-back-to-heading 'invisible-ok)
-                                                      t)
-                                                    (funcall predicate))
+              ;; `cl-loop' makes this double-while much clearer than the expanded form.
+              (cond (preamble-re (cl-loop while (and (when (re-search-forward preamble-re nil t)
+                                                       (outline-back-to-heading 'invisible-ok)
+                                                       t)
+                                                     (funcall predicate))
                                           collect (funcall action)
                                           while (outline-next-heading)))
                     (t (cl-loop when (funcall predicate)

commit f47856dc106dd9baa9e62fbd41544b1c90b10e80

Fix: Compound queries with preamble
Bug was introduced in 4e6bde8.

dir2/org-ql.el
@@ -416,12 +416,11 @@ If NARROW is non-nil, buffer will not be widened."
               (when (org-before-first-heading-p)
                 (outline-next-heading))
               ;; `cl-loop' makes this double-while much clearer than the expanded form.
-              (cond (preamble-re (cl-loop while (and (when (re-search-forward preamble-re nil t)
-                                                       (outline-back-to-heading 'invisible-ok)
-                                                       t)
-                                                     (funcall predicate))
+              (cond (preamble-re (cl-loop while (re-search-forward preamble-re nil t)
+                                          do (outline-back-to-heading 'invisible-ok)
+                                          when (funcall predicate)
                                           collect (funcall action)
-                                          while (outline-next-heading)))
+                                          do (outline-next-heading)))
                     (t (cl-loop when (funcall predicate)
                                 collect (funcall action)
                                 while (outline-next-heading)))))))

dir2/tests/test-org-ql.el
@@ -459,7 +459,16 @@ Based on Buttercup macro `it'."
         (expect (org-ql test-buffer
                   (ts :on "2019-06-09")
                   :action (org-ql-test-org-get-heading))
-                :to-equal nil)))))
+                :to-equal nil)))
+
+    (describe "Compound queries"
+
+      (org-ql-it "Tags and to-do"
+        (expect (org-ql test-buffer
+                  (and (todo "SOMEDAY")
+                       (tags "Emacs"))
+                  :action (org-ql-test-org-get-heading))
+                :to-equal '("Rewrite Emacs in Common Lisp"))))))
 
 ;; Local Variables:
 ;; truncate-lines: t