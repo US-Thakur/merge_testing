US-Thakur/merge_testing

commit 7c79a120d967a4178a20045bd485ea9f1bbffa16

Change/Fix: (org-ql-view--format-element) Use org-ql--tags-at
Use org-ql--tags-at instead of org-get-tags/org-get-tags-at.  This
uses our cache, which should improve performance, and it obviates the
function alias for Org versions before 9.3 (which occasionally
misbehaves, e.g. if Org is reloaded).

dir2/org-ql-view.el
@@ -47,11 +47,6 @@
 (require 'ov)
 (require 'ts)
 
-;;;; Compatibility
-
-(when (version< org-version "9.2")
-  (defalias 'org-get-tags #'org-get-tags-at))
-
 ;;;; Faces
 
 (defface org-ql-view-due-date
@@ -729,18 +724,16 @@ return an empty string."
                        (org-link-display-format it)))
            (todo-keyword (-some--> (org-element-property :todo-keyword element)
                                    (org-ql-view--add-todo-face it)))
-           ;; FIXME: Figure out whether I should use `org-agenda-use-tag-inheritance' or `org-use-tag-inheritance', etc.
            (tag-list (if org-use-tag-inheritance
-                         ;; FIXME: Note that tag inheritance cannot be used here unless markers are
-                         ;; added, otherwise we can't go to the item's buffer to look for inherited
-                         ;; tags.  (Or does `org-element-headline-parser' parse inherited tags too?  I
-                         ;; forget...)
+                         ;; MAYBE: Use our own variable instead of `org-use-tag-inheritance'.
                          (if-let ((marker (or (org-element-property :org-hd-marker element)
                                               (org-element-property :org-marker element))))
-                             (with-current-buffer (marker-buffer marker)
-                               ;; I wish `org-get-tags' used the correct buffer automatically.
-                               (org-get-tags marker (not org-use-tag-inheritance)))
+                             (cl-loop for type in (org-ql--tags-at marker)
+                                      unless (or (eq 'org-ql-nil type)
+                                                 (not type))
+                                      append type)
                            ;; No marker found
+                           ;; TODO: Use `display-warning' with `org-ql' as the type.
                            (warn "No marker found for item: %s" title)
                            (org-element-property :tags element))
                        (org-element-property :tags element)))

dir2/README.org
@@ -418,6 +418,9 @@ Expands into a call to ~org-ql-select~ with the same arguments.  For convenience
 *Changed*
 +  Binding to refresh search/view buffers changed to =r=.
 
+*Internal*
++  When formatting entries for Org QL View buffers, use internal function for retrieving heading tags.  This improves speed by using our cache, and it removes the need for a compatibility alias for Org versions before 9.3.
+
 ** 0.4.6
 
 *Fixed*

dir2/org-ql.info
@@ -772,6 +772,12 @@ File: README.info,  Node: 05-pre,  Next: 046,  Up: Changelog
    *Changed*
    • Binding to refresh search/view buffers changed to r.
 
+   *Internal*
+   • When formatting entries for Org QL View buffers, use internal
+     function for retrieving heading tags.  This improves speed by using
+     our cache, and it removes the need for a compatibility alias for
+     Org versions before 9.3.
+
 
 File: README.info,  Node: 046,  Next: 045,  Prev: 05-pre,  Up: Changelog
 
@@ -1205,25 +1211,25 @@ Node: Listing / acting-on results21208
 Node: Tips25810
 Node: Changelog26141
 Node: 05-pre26780
-Node: 04627369
-Node: 04527772
-Node: 04428131
-Node: 04328488
-Node: 04228683
-Node: 04128842
-Node: 0429081
-Node: 03233012
-Node: 03133389
-Node: 0333586
-Node: 02336561
-Node: 02236789
-Node: 02137057
-Node: 0237256
-Node: 0141291
-Node: Notes41392
-Node: Comparison with Org Agenda searches41554
-Node: org-sidebar42426
-Node: License42705
+Node: 04627626
+Node: 04528029
+Node: 04428388
+Node: 04328745
+Node: 04228940
+Node: 04129099
+Node: 0429338
+Node: 03233269
+Node: 03133646
+Node: 0333843
+Node: 02336818
+Node: 02237046
+Node: 02137314
+Node: 0237513
+Node: 0141548
+Node: Notes41649
+Node: Comparison with Org Agenda searches41811
+Node: org-sidebar42683
+Node: License42962
 
 End Tag Table
 

commit 6c4fe9323821bd4450b0339e8f0b084f69a4992d

Fix: (org-ql-view--format-element) Get tags from marker buffer
Fixes bug introduced in 7c79a120d967a4178a20045bd485ea9f1bbffa16.

Fixes #150.  Thanks to @tpeacock19 for reporting.

dir2/org-ql-view.el
@@ -837,10 +837,12 @@ return an empty string."
                          ;; MAYBE: Use our own variable instead of `org-use-tag-inheritance'.
                          (if-let ((marker (or (org-element-property :org-hd-marker element)
                                               (org-element-property :org-marker element))))
-                             (cl-loop for type in (org-ql--tags-at marker)
-                                      unless (or (eq 'org-ql-nil type)
-                                                 (not type))
-                                      append type)
+                             (with-current-buffer (marker-buffer marker)
+                               (org-with-wide-buffer
+                                (cl-loop for type in (org-ql--tags-at marker)
+                                         unless (or (eq 'org-ql-nil type)
+                                                    (not type))
+                                         append type)))
                            ;; No marker found
                            ;; TODO: Use `display-warning' with `org-ql' as the type.
                            (warn "No marker found for item: %s" title)